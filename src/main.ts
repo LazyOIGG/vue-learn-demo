b0ef456955f8c158e0538c053333a446a53d8854
LazyOIGG <nichunyang2005@foxmail.com>
2025-04-28 20:17:42 +0800

    空白项目

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,8 @@
 import '@unocss/reset/tailwind.css'
 import 'virtual:uno.css'
-
 import './assets/main.css'
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue' /*从已安装的软件包（vue）中导入调用函数*/
+import App from './App.vue' /*不带花括号则导入整个组件*/
 
-createApp(App).mount('#app')
+createApp(App).mount('#app') /*函数功能：创建一个App #标志去index.html中寻找id为App的标记*/
