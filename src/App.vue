c6f2cb3715f929af7b6c08bf0122f6c6a2b978dd
LazyOIGG <nichunyang2005@foxmail.com>
2025-05-07 19:35:07 +0800

    Version 1.0 简单改造，未使用Unocss

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,84 @@
-<script setup lang="ts">
-</script>
-
 <template>
+  <div class="container">
+    <div class="left">
+      <header>
+        <img class="avatar" src="/assets/images/avatar.jpg" alt="阿尔玛头像" />
+        <a class="name" href="https://www.monsterhunter.com/wilds/zh-hans/characters/">阿尔玛</a>
+        <div class="title">猎人公会助理</div>
+      </header>
 
+      <section class="contact">
+        <div><i class="icon icon-contact"></i>联系方式</div>
+        <div><i class="icon icon-call-me"></i>+86 12345678910</div>
+        <div><i class="icon icon-email"></i>alma@hunterguild.org</div>
+        <div><i class="icon icon-home"></i>天堑沙原大本营</div>
+      </section>
+
+      <section class="about-me">
+        <div><i class="icon icon-user"></i>自我介绍</div>
+        <div>
+          负责处理怪物狩猎的请求与许可，以及任务管理与承接的编辑者。在原野上也会与猎人同行，提供有助于狩猎的信息，还会在管理任务与补给等方面提供支援。专攻文化人类学，在考古学方面也有造诣。
+        </div>
+      </section>
+
+      <section class="skills">
+        <div><i class="icon icon-skills"></i>技能</div>
+        <div>
+          在原野上也会与猎人同行，提供有助于狩猎的信息，还会在管理任务与补给等方面提供支援。专攻文化人类学，在考古学方面也有造诣。
+        </div>
+      </section>
+    </div>
+
+    <div class="right">
+      <section class="education">
+        <h2><i class="icon icon-education"></i> 教育经历</h2>
+        <div class="education-item">
+          <h3>文化人类学硕士</h3>
+          <p><em>新大陆学术研究院</em> &nbsp; 2016 - 2020</p>
+          <p>在此期间，我深入研究了古代文明与怪物生态的关系，并参与多项考古发掘项目。</p>
+        </div>
+        <div class="education-item">
+          <h3>考古学博士</h3>
+          <p><em>龙历院高等学府</em> &nbsp; 2020 - 2023</p>
+          <p>在此期间，我专注于古龙种遗迹研究，并发表了多篇关于古代猎人文明的论文。</p>
+        </div>
+      </section>
+
+      <section class="experience">
+        <h2><i class="icon icon-experience"></i> 工作经历</h2>
+        <div class="experience-item">
+          <h3>猎人公会任务编辑者</h3>
+          <p><em>新大陆调查团</em> &nbsp; 2023 - 至今</p>
+          <p>负责处理怪物狩猎请求与许可，管理任务分配，并在野外为猎人提供战术支援与补给管理。</p>
+        </div>
+        <div class="experience-item">
+          <h3>考古队研究员</h3>
+          <p><em>龙历院考察队</em> &nbsp; 2019 - 2020</p>
+          <p>参与古代遗迹发掘工作，分析猎人文明的历史文献，协助解读古龙种生态。</p>
+        </div>
+      </section>
+
+      <section class="references">
+        <h2><i class="icon icon-references"></i> 推荐人</h2>
+        <div class="reference-item">
+          <p><strong>总司令</strong></p>
+          <p><em>新大陆调查团 / 总指挥</em></p>
+          <p><strong>电话：</strong>（新大陆专用通讯频段）</p>
+          <p><strong>邮箱：</strong>commander@newworld.org</p>
+        </div>
+        <div class="reference-item">
+          <p><strong>龙历院首席学者</strong></p>
+          <p><em>龙历院 / 学术主管</em></p>
+          <p><strong>电话：</strong>（龙历院内部线路）</p>
+          <p><strong>邮箱：</strong>scholar@dragonacademy.edu</p>
+        </div>
+      </section>
+    </div>
+  </div>
 </template>
 
-<style scoped>
+<script setup lang="ts">
+// 此处可以添加必要的逻辑，如响应式数据、方法等
+</script>
 
-</style>
+<style scoped src="./assets/styles.css"></style>
